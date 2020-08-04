ea249415e61d944b55b0d0cae531e230857a2197
pbove <771757562@qq.com>
2020-08-05 07:59:44 +0800

    修改makedown样式

--- plugins/editor.js
+++ plugins/editor.js
@@ -6,42 +6,13 @@ import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import VMdEditor from '@kangc/v-md-editor/lib/codemirror-editor';
 import '@kangc/v-md-editor/lib/style/codemirror-editor.css';
 import '@kangc/v-md-editor/lib/style/preview.css';
-import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
-// 高亮
-import typescript from 'highlight.js/lib/languages/typescript';
-import json from 'highlight.js/lib/languages/json';
-import cpp from 'highlight.js/lib/languages/cpp';
-import java from 'highlight.js/lib/languages/java';
-import nginx from 'highlight.js/lib/languages/nginx';
-import php from 'highlight.js/lib/languages/php';
-import python from 'highlight.js/lib/languages/python';
-import markdown from 'highlight.js/lib/languages/markdown';
-import dart from 'highlight.js/lib/languages/dart';
-import vim from 'highlight.js/lib/languages/vim';
-import sql from 'highlight.js/lib/languages/sql';
-import go from 'highlight.js/lib/languages/go';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
 
 // emoji
 import createEmojiPlugin from '@kangc/v-md-editor/lib/plugins/emoji/index';
 
 VMdEditor.use(createEmojiPlugin());
-VMdEditor.use(githubTheme, {
-  extend(_md, hljs) {
-    hljs.registerLanguage('ts', typescript);
-    hljs.registerLanguage('py', python);
-    hljs.registerLanguage('php', php);
-    hljs.registerLanguage('java', java);
-    hljs.registerLanguage('json', json);
-    hljs.registerLanguage('nginx', nginx);
-    hljs.registerLanguage('cpp', cpp);
-    hljs.registerLanguage('c', cpp);
-    hljs.registerLanguage('md', markdown);
-    hljs.registerLanguage('dart', dart);
-    hljs.registerLanguage('vm', vim);
-    hljs.registerLanguage('sql', sql);
-    hljs.registerLanguage('go', go);
-  },
-});
+VMdEditor.use(vuepressTheme);
 
 const tagList = [ 'input', 'style', 'script' ];
 
@@ -58,7 +29,7 @@ VMdPreview.xss.extend({
     return html;
   },
 });
-VMdPreview.use(githubTheme);
+VMdPreview.use(vuepressTheme);
 VMdPreview.use(createEmojiPlugin());
 
 Vue.use(VMdEditor);
